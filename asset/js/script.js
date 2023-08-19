753df577427f445b528c17d5f5442f1b126b940b
rpalmak <rpalmakaschel@gmail.com>
2023-08-18 22:38:45 -0400

    ejercicio 2 y 3

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,21 +1,42 @@
 
-
+/*-----------------------------------EJERCICIO 1------------------------------------------*/
 function imagenBorde() {
   const imagen = document.getElementsByTagName('img')[0];
   imagen.classList.toggle("border");
+}
+
+/*-----------------------------------EJERCICIO 2------------------------------------------*/
+
+const parrafo = document.getElementById('respuesta');
+
+function eliminarMensaje(){
+  setTimeout(function() {
+    parrafo.style.visibility = "hidden";
+  }, 5000);
 }
 
 function calcularStickers(){
   const cantidadSticker1 = Number(document.getElementById('cantidadSticker1').value);
   const cantidadSticker2 = Number(document.getElementById('cantidadSticker2').value);
   const cantidadSticker3 = Number(document.getElementById('cantidadSticker3').value);
-  let suma=cantidadSticker1+cantidadSticker2+cantidadSticker3;
-  if(suma<=10){
-    let parrafo = document.getElementById('respuesta');
-    parrafo.innerText="Llevas "+suma+" stickers";
+  if(cantidadSticker1 <=0 || cantidadSticker2 <=0 || cantidadSticker3 <=0){
+    parrafo.innerText="Debes ingresar valores válidos";
+    parrafo.style.visibility = "visible";
+    eliminarMensaje();
   }
   else{
-    alert("Llevas demasiados stickers")
+      let suma=cantidadSticker1+cantidadSticker2+cantidadSticker3;
+      if(suma<=10){
+        parrafo.innerText="Llevas "+suma+" stickers";
+        parrafo.style.visibility = "visible";
+        eliminarMensaje()
+      }
+      else{
+        parrafo.innerText="Llevas demasiados stickers";
+        parrafo.style.visibility = "visible";
+        eliminarMensaje()
+      }
   }
-  console.log(cantidadSticker1);
 }
+  
+/*-----------------------------------EJERCICIO 3------------------------------------------*/
