45afc5377d15c8d17112c807856e15061ae45cf1
Raúl Palma Kaschel <rpalmakaschel@gmail.com>
2023-08-24 22:59:51 -0400

    desafio semanal

---
+++ asset/js/1_funcion.js
@@ -0,0 +1,10 @@
+
+//funcion original
+function example(a, b, c){
+  return a+b+c
+  }
+
+//funcion transformada a función de expresion 
+  example = function(a,b,c){
+    return a+b+c
+  }
---
+++ asset/js/2_arrow.js
@@ -0,0 +1,10 @@
+
+//funcion original
+let suma = function(a, b){
+    return a + b
+    }
+
+//arrow function
+
+suma = (a,b) => a+b;
+    
