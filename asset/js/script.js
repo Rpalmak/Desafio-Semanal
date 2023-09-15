1e8e33c7bb398aafe1ef3388a50aa19868e7e6de
Raúl Palma Kaschel <rpalmakaschel@gmail.com>
2023-09-14 23:50:32 -0300

    avance

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,8 +1,34 @@
-async function consulta () {
+const url = "https://mindicador.cl/api/";
 
-  const respuesta = await fetch ("https://mindicador.cl/api/")
-  const data = await respuesta.json()
-  console.log(data)  
+
+
+const consulta = async () => {
+  try {
+    const monedaElegida = document.getElementById("moneda").value;
+    const respuesta = await fetch(url+monedaElegida)
+    const data = await respuesta.json()
+    if (!respuesta.ok) {
+      throw new Error("Error en la peticion");
+    }
+    const dataArray = Object.values(data)
+    const filterData = dataArray.filter((card) => card.imageUrl !== undefined);
+    renderGrafica();
   }
-  
-  getRandomUser()
+
+  catch (error) {
+    console.log(error);
+  }
+}
+
+
+async function renderGrafica() {
+  const data = await consulta();
+  const config = {
+    type: "line",
+    data
+  };
+  const myChart = document.getElementById("myChart");
+  myChart.style.backgroundColor = "white";
+  new Chart(myChart, config);
+}
+
